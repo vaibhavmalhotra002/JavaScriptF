bf40ddad40263fe6b232b6235c2e19f3fc9678d6
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2023-02-19 02:03:11 +0530

    commit changes

--- variable.js
+++ variable.js
@@ -2,5 +2,15 @@ console.log("This is jS")
 console.log('Even this works')
 let a = 78
 // console.log(a) 
-a = "vaibhav"
+// a = "vaibhav" 
 console.log(a) 
+
+let b="vaibhav"
+{
+    let b="tullu"
+    console.log(b)
+}
+console.log(b)
+const coder ='vaibhav'
+// coder='raj'
+console.log(coder)
