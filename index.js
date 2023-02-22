453777848a605f1215f9d908420764d4dc45a655
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2023-02-22 15:34:59 +0530

    changes

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // document.getElementById("count-el").innerText=18
-let firstBatch = 5
-let secondBatch = 6
+// let firstBatch = 5
+// let secondBatch = 6
 
-let count = firstBatch + secondBatch
-console.log(count);
+// let count = firstBatch + secondBatch
+// console.log(count);
+let lap1 = 34
+let lap2 = 35
+let lap3 = 33
+
+function sum() {
+    console.log(lap1+ lap2 +lap3)
+}
+sum()
