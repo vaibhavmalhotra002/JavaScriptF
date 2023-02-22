579cc6a66467263de69eb0e8e57f47e768a7f842
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2023-02-22 21:49:27 +0530

    commit

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 // document.getElementById("count-el").innerText=18
-// let firstBatch = 5
-// let secondBatch = 6
 
-// let count = firstBatch + secondBatch
-// console.log(count);
-let lap1 = 34
-let lap2 = 35
-let lap3 = 33
-
-function sum() {
-    console.log(lap1+ lap2 +lap3)
+// let countEl = document.getElementById("count-el")
+// let count = 0
+// function increment() {
+//     count = count + 1 
+//     countEl.innerText =count
+// }
+let countEl = document.getElementById("count-el")
+let count = 0 
+function increment(){
+    count =count + 1
+    countEl.innerText =count 
 }
-sum()
+
+function save(){
+    console.log(count)
+}
+save()
