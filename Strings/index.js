3f55d728d40b0e4e46917ccf13dbdceb7f391bb5
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2023-02-24 11:11:02 +0530

    changes.user

--- Strings/index.js
+++ Strings/index.js
@@ -1,6 +1,8 @@
 let welcomeEl = document.getElementById("welcome-el") 
 
-let name = 'vaibhav'
-let greeting = "Welcome Back"
+let name = 'User'
+let greeting = "Welcome Back "
 
 welcomeEl.innerText= greeting + name
+
+welcomeEl.innerText = welcomeEl.innerText + "🥳"
