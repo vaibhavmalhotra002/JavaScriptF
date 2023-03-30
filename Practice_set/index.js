585be48a93f4e5f99fcad50ad1a9992f4b723198
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2023-03-30 12:32:17 +0530

    commit changes

--- Practice_set/index.js
+++ Practice_set/index.js
@@ -1,6 +1,48 @@
+// Practice 1
+
+// Create two variables, firstName and lastName
 let firstname ="Vaibhav";
 let lastname ="Malhotra";
-
+//Concatenate the two variables into a third variable called fullName
 let fullname = firstname +" "+ lastname;
-
+// Log fullName to the console
 console.log(fullname)
+
+// Practice 2
+
+let name ="Vaibhav"
+let greeting ="Hi there"
+// Create a function that logs out "Hi there, Vaibhav!" when called
+function greetVaibhav() {
+  console.log(greeting + ", " + name + "!")
+}
+greetVaibhav();
+
+// Practice 4
+let myPoints = 3
+// Create two fupctions, addPoints() and removelPoint), and have them
+//add/remove points to/from the myPoints variable
+ function add3Points(){
+    myPoints += 3;
+ }
+ function remove1Point(){
+    myPoints -= 1;
+ }
+ // Call the functions to that the line below logs out 10
+ add3Points()
+ add3Points()
+ add3Points()
+ remove1Point()
+ remove1Point()
+
+ console.log(myPoints)
+
+
+ let errorParagraph = document.getElementById("error")
+ console.log (errorParagraph)
+
+ function Purchase(){
+    console.log ("Error")
+    // errorParagraph.textContent = "Something went wrong Please Try again"
+ }
+  
