1d0bdf8e32e6912e6fe7fe27a377c1ac8f5ac3d5
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2023-03-30 21:18:51 +0530

    commit changes

--- Calculator/index.js
+++ Calculator/index.js
@@ -2,8 +2,20 @@ let num1 = 8
 let num2 = 2
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
-
+let sumEl = document.getElementById("sum-el")
 // Create four functions: add(), subtract(), divide(), multiply()
+function add(){
+  sumEl.textContent = num1 + num2
+}
+function subtract(){
+
+}
+function divide(){
+
+}
+function multiply(){
+
+}
 // Call the correct function when the user clicks on one of the buttons
 // Perform the given calculation using num1 and num2
 // Render the result of the calculation in the paragraph with id="sum-el"
