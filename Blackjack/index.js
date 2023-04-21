db71f50c3b8583d45fcd687e31602e7f287459df
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2023-04-21 18:33:32 +0530

    commit

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -2,9 +2,35 @@
 // Set their values to a random number between 2-11
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
-let firstCard = 6
-let secondCard = 8
+let firstCard = 5
+
+let secondCard = 16
 
 let sum = firstCard + secondCard
 
-console.log(sum)
+if (sum <21){
+    console.log("Do you want to draw another card: 🫡")
+} else if (sum === 21){
+    console.log("Wooho You've got BlackJack!!🥳")
+} else {
+    console.log("You're out of the game😟")
+}
+
+// let age = 21
+
+// if (age<=21){
+//     console.log("You are not allowed to enter the NightClub")}
+//     else{
+//         console.log("You're allowed to enter the NightClub")
+//     }
+
+let age =100
+ if(age<100){
+    console.log("You are not eligible")
+ }
+ else if(age ===100){
+    console.log("Here is your birthday card from the king!")
+ }
+ else{
+    console.log("Not Eligible you have already gotten one")
+ }
